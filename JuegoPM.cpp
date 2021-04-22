1adb16abe3bbe1db5636f046260f1ff3bfaa5efb
Juan Pantaleon Femenia Quevedo <juanfemeniaquevedo@gmail.com>
2021-04-22 17:28:23 +0200

    Por revision de funciones de matrices

--- JuegoPM.cpp
+++ JuegoPM.cpp
@@ -7,14 +7,14 @@ const string msgerror = "Numero de intentos excedido";
 
 void banner(){		//done
 	cout << endl << endl;
-	cout << "$$$$$$$\                                $$\              " << endl;
-	cout << "$$  __$$\                               $$ |             " << endl;
-	cout << "$$ |  $$ |$$\   $$\ $$$$$$$$\ $$$$$$$$\ $$ |    $$$$$$\  " << endl;
-	cout << "$$$$$$$  |$$ |  $$ |\____$$  |\____$$  |$$ |  $$  __$$\  " << endl;
-	cout << "$$  ____/ $$ |  $$ |  $$$$ _/   $$$$ _/ $$ |  $$$$$$$$ | " << endl;
-	cout << "$$ |      $$ |  $$ | $$  _/    $$  _/   $$ |  $$   ____| " << endl;
-	cout << "$$ |      \$$$$$$  |$$$$$$$$\ $$$$$$$$\ $$ |  \$$$$$$$\  " << endl;
-	cout << "\__|       \______/ \________|\________|\__|   \_______| " << endl;
+	cout << "$$$$$$$\                                 $$\              " << endl;
+	cout << "$$  __$$\                                $$ |             " << endl;
+	cout << "$$ |  $$ |$$\    $$  \$$$$$$$$\  $$$$$$$$\  $$ |   $$$$$$\  " << endl;
+	cout << "$$$$$$$  |$$ |  $$ | \____$$  | \____$$  |$$ | $$   __$$\  " << endl;
+	cout << "$$  ____/ $$ |  $$ |   $$$$ _/   $$$$ _/$$ |$$$$$$$$| " << endl;
+	cout << "$$ |      $$ |  $$ |  $$  _/    $$  _/  $$ |$$   ____| " << endl;
+	cout << "$$ |      \$$$$$$  | $$$$$$$$\ $$$$$$$$\   $$ | \$$$$$$$\  " << endl;
+	cout << "\__|       \______/  \________|\________|\__|     \_______| " << endl;
 	cout << "                                                         " << endl;
 	cout << "                                                         " << endl;
 	cout << "                                                         " << endl;
@@ -27,10 +27,10 @@ int menu() {		//done
 	banner();
 	int opcion;
 	do {
-		cout << "Elija una opcion." << endl << endl;
-		cout << "1. Version de juego - 1D" << endl;
-		cout << "2. version de juego - 2D" << endl;
-		cout << "0. Salir " << endl;
+		cout << "     Elija una opcion." << endl << endl;
+		cout << "     1. Version de juego - 1D" << endl;
+		cout << "     2. version de juego - 2D" << endl;
+		cout << "     0. Salir " << endl;
 		cin >> opcion;
 
 	} while (opcion > 2 || opcion < 0);
@@ -51,7 +51,7 @@ void mainJuegoPM() {		//done
 		case 1:
 			juegoCorrecto = iniciar(juego, "1D");
 			if (!juegoCorrecto) {
-				cout << "Error al iniciar el juego..." << endl;
+				cout << "   Error al iniciar el juego..." << endl;
 			}
 			if (!jugar(juego)) {
 				cout << msgerror << endl;
@@ -61,7 +61,7 @@ void mainJuegoPM() {		//done
 		case 2:
 			juegoCorrecto = iniciar(juego, "2D");
 			if (!juegoCorrecto) {
-				cout << "Error al iniciar el juego..." << endl;
+				cout << "   Error al iniciar el juego..." << endl;
 			}
 			if (!jugar(juego)) {
 				cout << msgerror << endl;
@@ -84,7 +84,7 @@ bool iniciar(tJuegoPM& jpm, string modo) {		//done
 	}
 	else
 	{
-		cout << "No se ha cargado el juego..." << endl;
+		cout << "  No se ha cargado el juego..." << endl;
 	}
 
 
@@ -98,7 +98,7 @@ bool cargar(tJuegoPM& jpm) {	//done revisada
 	
 	
 
-	cout << "Nombre del archivo:" << endl;//(aqui es donde se pide el nombre de archivo y se abre el correspondiente).
+	cout << "   Nombre del archivo:" << endl;//(aqui es donde se pide el nombre de archivo y se abre el correspondiente).
 	cin >> nombre;
 	archivo.open(nombre);		//con el .txt(obligatorio)
 	if (!archivo.is_open()) {
@@ -127,10 +127,10 @@ bool cargar(tJuegoPM& jpm) {	//done revisada
 }
 
 void mostrar(const tJuegoPM& jpm) {		//done
-	cout << endl << "Numero de intentos: " << jpm.num << endl;
-	cout << "Imagen original:" << endl;
+	cout << endl << "   Numero de intentos: " << jpm.num << endl << endl;
+	cout << "    Imagen original:" << endl;
 	printMatriz(jpm.matIni);
-	cout << "Imagen final: " << endl;
+	cout << "   Imagen final: " << endl;
 	printMatriz(jpm.matFin);
 }
 
@@ -159,71 +159,79 @@ bool juegoGanado(const tJuegoPM& jpm) {		//funcion de logica especial propia par
 }
 
 bool accion(tJuegoPM& jpm) {
-	cout << endl << "Introduzca un comando a realizar:   " << endl << endl;
+	cout << endl << "   Introduzca un comando a realizar:   " << endl << endl;
 	
 	bool accionado = false;
 	string ac;
 	int f1, f2, c1, c2;
 	if (jpm.tipo == "1D") {
 		cin >> ac;
-		if (ac == "SF") {
-			cout << "Que columna quieres voltear: " << endl;
+		if (ac == "SF") {		//funciona correctamente el comando
+			cout << "   Que columna quieres voltear: " << endl;
 			cin >> f1 >> f2;
 			swapF(jpm.matIni, f1, f2);
 		}
-		else if (ac == "SC") {
-			cout << "Que columna quieres voltear: " << endl;
+		else if (ac == "SC") {		//no funciona correctamente(revisar)
+			cout << "   Que columna quieres voltear: " << endl;
 			cin >> c1 >> c2;
 			swapC(jpm.matIni, c1, c2);
 		}
-		else if (ac == "VF") {
-			cout << "Que columna quieres voltear: " << endl;
+		else if (ac == "VF") {		//no funciona correctamente(revisar)
+			cout << "   Que columna quieres voltear: " << endl;
 			cin >> f1;
 			voltearF(jpm.matIni, f1);
 		}
-		else if (ac == "SD") {
-			cout << "Que columna quieres voltear: " << endl;
+		else if (ac == "SD") {		//no funciona correctamente(revisar)
+			cout << "   Que columna quieres voltear: " << endl;
 			cin >> f1;
 			swapD(jpm.matIni, f1);
 		}
-		else if (ac == "VC") {
-			cout << "Que columna quieres voltear: " << endl;
+		else if (ac == "VC") {	    //no funciona correctamente(revisar)
+			cout << "   Que columna quieres voltear: " << endl;
 			cin >> f1;
 			voltearC(jpm.matIni, f1);
 		}
-		else if (ac == "VD") {
-			cout << "Que diagnolal desea voltear: " << endl;
+		else if (ac == "VD") {		//no funciona correctamente(revisar)
+			cout << "   Que diagnolal desea voltear: " << endl;
 			cin >> f1;
 			voltearD(jpm.matIni, f1);
+
 		}
 	}
 	else if (jpm.tipo == "2D") {
 		cin >> ac;
-		if (ac == "VV") {
-			cout << "Que columna quieres voltear: ";
-			cin >> c1;
-		    voltearC(jpm.matIni, c1);
+		if (ac == "VV") {			//no funciona correctamente(revisar)
+			//esta funcion VV no requiere de parametros revisar pdf practica		
+			//voltearV(jpm.matIni);		//falta por hacer(no se ha hecho ni creado)
 		}
-		else if (ac == "VH") {
-			cout << "Que fila quieres voltear: ";
-			cin >> f1;
-			voltearF(jpm.matIni, f1);
+		else if (ac == "VH") {		//no funciona correctamente(revisar)
+			//esta funcion VH no requiere de parametros revisar pdf practica		
+			//void voltearH(jpm.matIni):	//falta por hacer(no se ha hecho ni creado)
+			
 		}
 		else if (ac == "RD") {		//funciona correctamente
 			rotarD(jpm.matIni);
 
 		}
-		else if (ac == "SA") {
+		else if (ac == "SA") {		//no funciona correctamente(revisar)
 			tCoor a,b;
-			cout << "Casilla 1: " << endl;
+			cout << "   Casilla 1: " << endl;
 			cin >> a.coordenadaX;
 			cin >> a.coordenadaY;
-			cout << endl << "Casilla 2: ";
+			cout << endl << "   Casilla 2: ";
 			cin >> b.coordenadaX;
 			cin >> b.coordenadaY;
-			swapAdy(jpm.matIni, a, b);
+			swapAdy(jpm.matIni, a, b);		//esta funcion la cree yo,de alguna forma aqui no funciona revisar.
 			
 		}
+		else if (ac =="VD"){	
+			// VoltearID(jpm.matIni);  //falta por hacer(no se ha hecho ni creado)
+
+
+		}
+		
+
+		
 	}
 	return accionado;
 }
